aea0959373e31e62a1ce79b764a2cf742cbc34a3
John Aldrich Bernardo <4ldrich@protonmail.com>
2020-09-23 10:48:04 +0800

    fix for pascal case

--- 3 - Style Guide.js
+++ 3 - Style Guide.js
@@ -12,4 +12,4 @@ var apples = 50         // Declaring single word identifier
 var applesCount = 50    // Camel Case -- Declaring multi-word identifier
 var apples_count = 50   // Underscore Case -- Declaring multi-word identifier
 var ApplesCount = 50    // Pascal Case -- Declaring multi-word identifier /
-                        // Pascal Case is not recommended for declaring functions or classes
+                        // Pascal Case is only recommended for declaring functions or classes
