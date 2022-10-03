9c82dba106012a4f37b43cc2a8a2d46370dce80a
John Aldrich Bernardo <4ldrich@protonmail.com>
2022-10-03 21:23:25 +0800

    added variables activity

---
+++ Crash Course/4 - Variables - Activity.js
@@ -0,0 +1,14 @@
+/**
+ * Declare constants for:
+ *  - Your Name
+ *  - Age
+ *  - Address (An object that has...)
+ *    - Street
+ *    - Barangay
+ *    - Municipality
+ *    - Province
+ *
+ * Using console.log print the values in the format:
+ *  Hello, I'm {Your Name}. Age {Age}. Living in {Street}, {Barangay}, {Municipality} of {Province}
+ *
+ */
