278040b8cc1b3dba579ebb2abd6b20e1259e49fc
John Aldrich Bernardo <4ldrich@protonmail.com>
2022-10-04 20:39:56 +0800

    added activities

---
+++ Crash Course/7 - Functions - Activity.js
@@ -0,0 +1,8 @@
+/**
+ * Create the function "computeDogAge" that would accepts a number, which
+ * represents the rate in human years (1 human year = 7 dog years). The
+ * function should return "Your dog is {NN} years old!"
+ *
+ * Call the function 3 times passing different set of values
+ *
+ */
---
+++ Crash Course/9 - Arrrays - Activity.js
@@ -0,0 +1,22 @@
+/**
+ * Create a function that would accept a list of objects.
+ * Each object in the list is a representation of a cart item.
+ *
+ * The function must iterate the list and compute for the sum of cart items.
+ *
+ * Given that the object could contain the following information:
+ *
+ *      [
+ *        {
+ *          "product": "Panda Ballpoint Pen",
+ *          "price": 5.50,
+ *          "quantity": 3
+ *        },
+ *        {
+ *          "product": "Yellow Pad Paper",
+ *          "price": 46.75,
+ *          "quantity": 4
+ *        }
+ *      ]
+ *
+ */
